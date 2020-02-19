6d66474b822929b81dd0c0d84c63a17616dba7cc
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-02-19 16:53:00 +0200

    commit 19.02 v2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -83,3 +83,69 @@
 //         }
 //     }
 // })();
+
+
+let variableImage = $(".image"),
+    idSlider = $("#slider");
+
+$(document).ready(function(){
+    variableImage.width(idSlider.width()/5);
+    $(".image:first-child").width(idSlider.width()/2);
+
+    // $(window).resize(function(){
+    //     variableImage.width(idSlider.width()/5);
+    //     $(".image:first-child").width(idSlider.width()/2);
+    // });
+
+    $(".item-img-1").each(function(){
+        let src = $(this).attr("data-src");
+        $(this).css({
+            background:`url(assets/img/${src}) no-repeat`,
+        })
+    });
+
+    variableImage.mouseover(function(){
+        $(this).find(".item-img-1").css({
+            opacity:1,
+        });
+
+        variableImage.width(idSlider.width()/4);
+        variableImage.css({"z-index":"-999px"});
+
+        $(this).css({"z-index":"999px"});
+        $(this).width(idSlider.width()/2)
+    });
+
+    variableImage.mouseout(function(){
+        $(this).find(".item-img-1").css({
+            opacity:1
+        });
+
+        variableImage.width(idSlider.width()/5);
+        $(".image:first-child").width(idSlider.width()/2);
+        $(this).css({"z-index":0})
+    });
+
+    $(".image:last-child").mouseover(function(){
+        $(this).find(".item-img-1").css({
+            opacity: 1,
+        });
+        variableImage.width(idSlider.width()/5);
+        variableImage.css({"z-index":"-999px"});
+
+        $(this).css({"z-index":"999px"});
+        $(this).width(idSlider.width()/1.5)
+    });
+
+    $(".image:first-child").mouseover(function(){
+        $(this).find(".item-img-1").css({
+            opacity: 1,
+        });
+        variableImage.css({"z-index":"-999px"});
+
+        $(this).css({"z-index":"999px"});
+
+        variableImage.width(idSlider.width()/5);
+        $(".image:first-child").width(idSlider.width()/2);
+    });
+});
