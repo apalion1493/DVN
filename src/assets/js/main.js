a1dd39a43c15dcd2cf560eee9fc1cdf906904659
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-02-25 15:47:07 +0200

    commit 25.02 v10

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,61 +1,116 @@
 let variableImage = $(".image"),
     idSlider = $("#slider");
 
-$(document).ready(function(){
-    variableImage.width(idSlider.width()/5);
-    $(".image:first-child").width(idSlider.width()/2);
+let isMobile = false;
+$(document).ready( function() {
+    if ($('body').width() <= 767.98) {
+        isMobile = true;
+    }
 
-    $(".item-img-1").each(function(){
-        let src = $(this).attr("data-src");
-        $(this).css({
-            background:`url(assets/img/${src}) no-repeat`,
-        })
-    });
+    if (isMobile) {
+        variableImage.width(idSlider.width()/5);
+        $(".image:first-child").width(idSlider.width()/1.5);
 
-    variableImage.mouseover(function(){
-        $(this).find(".item-img-1").css({
-            opacity:1,
+        $(".item-img-1").each(function(){
+            let src = $(this).attr("data-src");
+            $(this).css({
+                background:`url(assets/img/${src}) no-repeat`,
+            })
         });
+        variableImage.mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity:1,
+            });
 
-        variableImage.width(idSlider.width()/4);
-        variableImage.css({"z-index":"-999px"});
-
-        $(this).css({"z-index":"999px"});
-        $(this).width(idSlider.width()/3)
-    });
-
-    variableImage.mouseout(function(){
-        $(this).find(".item-img-1").css({
-            opacity:1
+            variableImage.width(idSlider.width()/1.1);
+            variableImage.css({"z-index":"-999px"});
+            $(".image:first-child").width(idSlider.width()/5);
+            $(this).css({"z-index":"999px"});
+            $(this).width(idSlider.width()/7)
         });
+        variableImage.mouseout(function(){
+            $(this).find(".item-img-1").css({
+                opacity:1
+            });
 
+            variableImage.width(idSlider.width()/5);
+            $(".image:first-child").width(idSlider.width()/1.5);
+            $(this).css({"z-index":0})
+        });
+        $(".image:last-child").mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity: 1,
+            });
+            variableImage.width(idSlider.width()/5);
+            variableImage.css({"z-index":"-999px"});
+
+            $(this).css({"z-index":"999px"});
+            $(this).width(idSlider.width()/2)
+        });
+        $(".image:first-child").mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity: 1,
+            });
+            variableImage.css({"z-index":"-999px"});
+
+            $(this).css({"z-index":"999px"});
+
+            variableImage.width(idSlider.width()/7);
+            $(".image:first-child").width(idSlider.width()/1.1);
+        });
+    }
+
+    if (!isMobile) {
         variableImage.width(idSlider.width()/5);
         $(".image:first-child").width(idSlider.width()/2);
-        $(this).css({"z-index":0})
-    });
-
-    $(".image:last-child").mouseover(function(){
-        $(this).find(".item-img-1").css({
-            opacity: 1,
+        $(".item-img-1").each(function(){
+            let src = $(this).attr("data-src");
+            $(this).css({
+                background:`url(assets/img/${src}) no-repeat`,
+            })
         });
-        variableImage.width(idSlider.width()/5);
-        variableImage.css({"z-index":"-999px"});
+        variableImage.mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity:1,
+            });
 
-        $(this).css({"z-index":"999px"});
-        $(this).width(idSlider.width()/2)
-    });
+            variableImage.width(idSlider.width()/4);
+            variableImage.css({"z-index":"-999px"});
 
-    $(".image:first-child").mouseover(function(){
-        $(this).find(".item-img-1").css({
-            opacity: 1,
+            $(this).css({"z-index":"999px"});
+            $(this).width(idSlider.width()/3)
         });
-        variableImage.css({"z-index":"-999px"});
+        variableImage.mouseout(function(){
+            $(this).find(".item-img-1").css({
+                opacity:1
+            });
 
-        $(this).css({"z-index":"999px"});
+            variableImage.width(idSlider.width()/5);
+            $(".image:first-child").width(idSlider.width()/2);
+            $(this).css({"z-index":0})
+        });
+        $(".image:last-child").mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity: 1,
+            });
+            variableImage.width(idSlider.width()/5);
+            variableImage.css({"z-index":"-999px"});
 
-        variableImage.width(idSlider.width()/5);
-        $(".image:first-child").width(idSlider.width()/2);
-    });
+            $(this).css({"z-index":"999px"});
+            $(this).width(idSlider.width()/2)
+        });
+        $(".image:first-child").mouseover(function(){
+            $(this).find(".item-img-1").css({
+                opacity: 1,
+            });
+            variableImage.css({"z-index":"-999px"});
+
+            $(this).css({"z-index":"999px"});
+
+            variableImage.width(idSlider.width()/5);
+            $(".image:first-child").width(idSlider.width()/2);
+        });
+    }
 });
 
 $('.main-navigation').click(function () {
